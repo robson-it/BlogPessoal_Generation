65165f53dadb06b740be2e63b97c8e5ce94576c4
Robson Alves Rocha <robson_it@outlook.com>
2023-10-09 15:48:06 -0300

    Update AppDbContext.cs - Remoção do timespan

--- BlogPessoal/Data/AppDbContext.cs
+++ BlogPessoal/Data/AppDbContext.cs
@@ -45,7 +45,7 @@ namespace BlogPessoal.Data
                 //Se uma propriedade da Classe Auditable estiver sendo criada. 
                 if (insertedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = new DateTimeOffset(DateTime.Now, new TimeSpan(-3, 0, 0));
+                    auditableEntity.Data = new DateTimeOffset(DateTime.Now);
                 }
             }
 
@@ -58,7 +58,7 @@ namespace BlogPessoal.Data
                 //Se uma propriedade da Classe Auditable estiver sendo atualizada.  
                 if (modifiedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = new DateTimeOffset(DateTime.Now, new TimeSpan(-3, 0, 0));
+                    auditableEntity.Data = new DateTimeOffset(DateTime.Now);
                 }
             }
 
